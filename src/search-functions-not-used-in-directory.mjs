4d6f4cd52c728d9aa15668800204fdef7349ea2d
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-08-02 13:43:45 +0200

    feat: save result

--- src/search-functions-not-used-in-directory.mjs
+++ src/search-functions-not-used-in-directory.mjs
@@ -4,6 +4,30 @@ import {parse} from '@babel/parser';
 import _traverse from '@babel/traverse';
 
 const traverse = _traverse.default;
+const __dirname = new URL('.', import.meta.url).pathname;
+
+export function searchFunctionsNotUsedInDirectory(searchFolderPath, functionsFolderPath) {
+  const exportedFunctions = getAllExportedFunctionsInDirectory(functionsFolderPath);
+  const result = [];
+  exportedFunctions.forEach(({fileName, functionName}) => {
+    const isCalled = isCalledInDirectory(searchFolderPath, {fileName, functionName});
+    if (!isCalled) {
+      const currentFile = result.find((r) => r.fileName === fileName);
+      if (currentFile) {
+        currentFile.functions.push(functionName);
+      } else {
+        result.push({fileName, functions: [functionName]});
+      }
+    }
+  });
+
+  saveResult(result, searchFolderPath, functionsFolderPath);
+}
+
+function getAllExportedFunctionsInDirectory(dirPath) {
+  const filePaths = getAllFilePathsInDirectory(dirPath);
+  return filePaths.flatMap((filePath) => getExportedFunctionsInFile(filePath));
+}
 
 function getAllFilePathsInDirectory(dirPath) {
   const files = fs.readdirSync(dirPath, {recursive: true});
@@ -13,11 +37,6 @@ function getAllFilePathsInDirectory(dirPath) {
       return fs.statSync(filePath).isFile() && path.extname(filePath) === '.js';
     })
     .map((file) => path.join(dirPath, file));
-}
-
-function getAllExportedFunctionsInDirectory(dirPath) {
-  const filePaths = getAllFilePathsInDirectory(dirPath);
-  return filePaths.flatMap((filePath) => getExportedFunctionsInFile(filePath));
 }
 
 function getExportedFunctionsInFile(filePath) {
@@ -75,20 +94,14 @@ function isCalledInFile(filePath, {fileName, functionName}) {
   return functionCalled;
 }
 
-export function searchFunctionsNotUsedInDirectory(searchFolderPath, functionsFolderPath) {
-  const exportedFunctions = getAllExportedFunctionsInDirectory(functionsFolderPath);
-  const results = new Map();
-  let n = 0;
-  exportedFunctions.forEach(({fileName, functionName}) => {
-    const isCalled = isCalledInDirectory(searchFolderPath, {fileName, functionName});
-    if (!isCalled) {
-      n++;
-      if (!results.has(fileName)) {
-        results.set(fileName, [functionName]);
-      } else {
-        results.get(fileName).push(functionName);
-      }
-    }
-  });
-  console.log(`Il y a ${n} fonctions non utilisées dans le dossier ${searchFolderPath}:`);
+function saveResult(result, searchFolderPath, functionsFolderPath) {
+  const fileName = `last-run-${path.basename(searchFolderPath)}-for-${path.basename(functionsFolderPath)}.json`;
+  const filePath = path.join(__dirname, '../data', fileName);
+  fs.writeFileSync(filePath, JSON.stringify(result));
+
+  const historyFileName = `history-${path.basename(searchFolderPath)}-for-${path.basename(functionsFolderPath)}.json`;
+  const historyFilePath = path.join(__dirname, '../data', historyFileName);
+  const history = fs.existsSync(historyFilePath) ? JSON.parse(fs.readFileSync(historyFilePath, 'utf-8')) : [];
+  const notUsedFunctions = result.flatMap((r) => r.functions).length;
+  fs.writeFileSync(historyFilePath, JSON.stringify([...history, {date: new Date(), notUsedFunctions}]));
 }
