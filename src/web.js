34921d31ce3b0831a9a7708efe5ca453c85fc4f0
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-08-03 09:42:38 +0200

    feat: add web

---
+++ src/web.js
@@ -0,0 +1,33 @@
+import DataTable from 'datatables.net-bs'
+import 'datatables.net-bs/css/dataTables.bootstrap.css'
+import lastRun from '../data/last-run-usecases-for-repositories.json'
+
+let table
+
+async function displayLastRun() {
+  if (table)
+    table.destroy()
+
+  table = new DataTable('#rawResult', {
+    paging: false,
+    searching: false,
+    info: false,
+    columns: [
+      { data: 'fileName', title: 'Repositories' },
+      {
+        data: 'functions',
+        title: 'Functions',
+        render(data, type, row) {
+          return type === 'display' ? data.join('<br>') : data
+        },
+      },
+    ],
+    data: lastRun,
+  })
+}
+
+function main() {
+  displayLastRun()
+}
+
+main()
